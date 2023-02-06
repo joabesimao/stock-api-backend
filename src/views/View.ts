cd026930305ab015c0c0365fa4efd3f9b523ab93
joabesimao <simaojoabecosta@gmail.com>
2023-02-06 18:47:07 -0300

    cria classes

---
+++ src/views/View.ts
@@ -0,0 +1,13 @@
+export abstract class View<T> {
+  protected element: HTMLElement;
+
+  constructor(select: string) {
+    this.element = document.querySelector(select);
+  }
+  public update(model: T): void {
+    let template = this.template(model);
+
+    this.element.innerHTML = template;
+  }
+  protected abstract template(model: T): string;
+}
