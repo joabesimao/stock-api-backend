c37138b0da26b0f8e5b4b7e2fc4b40adc1afe9e0
joabesimao <simaojoabecosta@gmail.com>
2023-02-08 20:02:43 -0300

    implementa corretamente a class Position

--- src/domain/entities/Position.ts
+++ src/domain/entities/Position.ts
@@ -1,32 +1,12 @@
-import { Money } from './Money';
 import { Stock } from './Stock';
 
-export class Position implements Stock {
-  constructor(
-    private _stock: Stock,
-    private _quantity: number,
-    public _id: string,
-    public _name: string,
-    public _valueStock: Money
-  ) {}
-
+export class Position {
+  constructor(private _stock: Stock, private _quantity: number) {}
   public get quantity(): number {
     return this._quantity;
   }
-  public set quantity(value: number) {
-    this._quantity = value;
-  }
+
   public get stock(): Stock {
     return this._stock;
-  }
-  public set stock(value: Stock) {
-    this._stock = value;
-  }
-
-  showIdENameStock(): string {
-    return `${this._id} , ${this._name}`;
-  }
-  showValueStock(): Money {
-    return this._valueStock;
   }
 }
